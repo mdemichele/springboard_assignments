27bd31ec69217db83acc63662d9c56c5b8e7cb4b
Matthew DeMichele <Matt@Matthews-MacBook-Air.local>
2021-10-21 14:33:18 -1000

    finished 3 more exercises

--- 18.2_python_data_structures/07_multiple_letter_count/multiple_letter_count.py
+++ 18.2_python_data_structures/07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,12 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    dictionary = {}
+    
+    for curr in phrase:
+        if dictionary.get(curr) != None:
+            dictionary[curr] = dictionary[curr] + 1
+        else:
+            dictionary[curr] = 1
+
+    return dictionary
